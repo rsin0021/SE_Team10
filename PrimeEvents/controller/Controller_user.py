df313ce2139e5e94480751c5e4dd7d05a5cffe04
Yumeng Zhao <yzha0448@student.monash.edu>
2019-10-09 11:41:10 +1100

    view hall & search hall V1.0

--- PrimeEvents/controller/Controller_user.py
+++ PrimeEvents/controller/Controller_user.py
@@ -5,17 +5,19 @@ class Controller_user:
     def __init__(self):
         pass
 
-    def check_input(self):
+    def check_input(self,df):
         operation = input("enter N to next page" + '\n' + "enter B to exit" + '\n' +
                           "enter index to select hall" + '\n' + "enter F to search hall" + '\n')
         if operation == 'n' or operation == 'N':
             return 'N'
         elif operation == 'b' or operation == 'B':
             return 'B'
-        elif operation in range(0,self.view_hall().df.shape[0] + 1):
+        elif operation == 'f' or operation == 'F':
+            return 'F'
+        elif operation in range(0,df.shape[0] + 1):
             return operation
         else:
-            print("please ensure vaild input")
+            print("Please ensure vaild input" + '\n')
             self.check_input()
 
     def view_hall(self):
@@ -25,14 +27,27 @@ class Controller_user:
                 print(df[int(df.shape[0] / 10) * 10:])
             else:
                 print(df[(i - 1) * 10:i * 10])
-            operation = self.check_input()
+            operation = self.check_input(df)
             if operation == 'N':
                 continue
             elif operation == 'B':
-                return
+                return                             # go back to the previous page
+            elif operation == 'F':
+                self.search_hall()
             else:
                 return df.iat[operation,1]   # return hall_id
-        self.check_input()
+        self.view_hall()
 
     def search_hall(self):
-        pass
+        df = pd.read_csv('Hall_db.csv')
+        address = input("Enter address information to search: " + '\n')
+        res = df.loc[df['Hall_description'].str.contains(address)]
+        if res.shape[0] == 0:
+            print("No correspond information is found" + '\n')
+            self.search_hall()
+        print(res)
+        operation = input("enter B to go back" + '\n' + "enter index to select hall" + '\n')
+        if operation in range(0,res.shape[0] + 1):
+            return operation
+        else:
+            return                             # go back to the previous page
