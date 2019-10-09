b8b21d2541c6a8ecd9ad15cab0e278b864215238
Yumeng Zhao <yzha0448@student.monash.edu>
2019-10-09 12:50:47 +1100

    view hall & search hall V2.0
    Debug done.

--- PrimeEvents/controller/Controller_user.py
+++ PrimeEvents/controller/Controller_user.py
@@ -5,49 +5,60 @@ class Controller_user:
     def __init__(self):
         pass
 
-    def check_input(self,df):
+    def check_input(self):
+        df = pd.read_csv('../table/halls.csv')
         operation = input("enter N to next page" + '\n' + "enter B to exit" + '\n' +
                           "enter index to select hall" + '\n' + "enter F to search hall" + '\n')
-        if operation == 'n' or operation == 'N':
+        if operation == 'N':
             return 'N'
-        elif operation == 'b' or operation == 'B':
+        elif operation == 'B':
             return 'B'
-        elif operation == 'f' or operation == 'F':
+        elif operation == 'F':
             return 'F'
-        elif operation in range(0,df.shape[0] + 1):
+        elif int(operation) in range(0,df.shape[0]):
+
             return operation
         else:
             print("Please ensure vaild input" + '\n')
-            self.check_input()
+            return self.check_input()
 
     def view_hall(self):
-        df = pd.read_csv('Hall_db.csv')
+        df = pd.read_csv('../table/halls.csv')
         for i in range(1, int(df.shape[0] / 10) + 2):  # for each page, only display 10 halls
             if i == int(df.shape[0] / 10) + 1:
                 print(df[int(df.shape[0] / 10) * 10:])
             else:
                 print(df[(i - 1) * 10:i * 10])
-            operation = self.check_input(df)
+            operation = self.check_input()
             if operation == 'N':
                 continue
             elif operation == 'B':
                 return                             # go back to the previous page
             elif operation == 'F':
-                self.search_hall()
+                res = self.search_hall()
+                if res != None:
+                    return res
             else:
-                return df.iat[operation,1]   # return hall_id
+                return df.iat[int(operation),1]   # return hall_id
         self.view_hall()
 
     def search_hall(self):
-        df = pd.read_csv('Hall_db.csv')
+        df = pd.read_csv('../table/halls.csv')
         address = input("Enter address information to search: " + '\n')
         res = df.loc[df['Hall_description'].str.contains(address)]
         if res.shape[0] == 0:
             print("No correspond information is found" + '\n')
             self.search_hall()
         print(res)
-        operation = input("enter B to go back" + '\n' + "enter index to select hall" + '\n')
-        if operation in range(0,res.shape[0] + 1):
-            return operation
+        operation = input("enter index to select hall" + '\n' + "enter any other to go back" + '\n')
+        try:
+            var = int(operation)
+        except ValueError:
+            return               # go back to the previous page
+        if int(operation) in range(0,res.shape[0]):
+            return res.iat[int(operation),1]
         else:
-            return                             # go back to the previous page
+            return
+
+a = Controller_user()
+a.view_hall()
